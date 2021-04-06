b224ed9883bdcc3dd40a81057b6696506064cf7e
Artyom-IWT <untila_artem@mail.ru>
2021-04-06 17:30:38 +0300

    ...

--- src/main/java/jcc/Cov1.java
+++ src/main/java/jcc/Cov1.java
@@ -1,14 +1,19 @@
 package jcc;
 
 import org.apache.tools.ant.DirectoryScanner;
-import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
+import org.jacoco.core.analysis.Analyzer;
+import org.jacoco.core.analysis.CoverageBuilder;
+import org.jacoco.core.data.ExecutionDataStore;
+import org.jacoco.core.data.SessionInfoStore;
+import org.jacoco.core.instr.Instrumenter;
+import org.jacoco.core.runtime.IRuntime;
+import org.jacoco.core.runtime.LoggerRuntime;
+import org.jacoco.core.runtime.RuntimeData;
 
 import java.io.File;
-import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.LinkedList;
 import java.util.List;
@@ -17,7 +22,7 @@ import java.util.jar.JarFile;
 
 public class Cov1 {
 
-    public static void main(String[] args) throws ClassNotFoundException, IOException {
+    public static void main(String[] args) throws Exception {
         if (args == null || args.length < 1)
             throw new IllegalArgumentException("Incorrect data entered");
 
@@ -52,10 +57,10 @@ public class Cov1 {
                 JarEntry je = e.nextElement();
                 String name = je.getName();
                 if (name.endsWith(".class") && name.contains("tests/")) {
-                    System.out.println(name);
+                    System.out.println(name); //"/tests/TestAdder.class"
+                    //"tests.TestAdder"
                     String className = name.substring(0, je.getName().length() - 6).replace('/', '.');
                     System.out.println(className);
-                    Class<?> c = cl.loadClass(className);
                     /*Arrays.stream(c.getMethods()).forEach(System.out::println);
                     for (Method m : c.getMethods()) {
                         for (Annotation a : m.getAnnotations()) {
@@ -64,12 +69,41 @@ public class Cov1 {
                             }
                         }
                     }*/
-                    System.out.println("\nRunning test...\n");
-                    Result result = JUnitCore.runClasses(c);
-                    result.getFailures().forEach(System.out::println);
+                    //System.out.println("\nRunning test...\n");
+
+
+                    final IRuntime runtime = new LoggerRuntime();
+
+                    //Instrumenter instr = new Instrumenter(runtime);
+                    //InputStream original = new Cov1().getClass().getResourceAsStream(name);
+                    //final byte[] instrumented = instr.instrument(original, className);
+                    //original.close();
+
+                    final RuntimeData data = new RuntimeData();
+                    runtime.startup(data);
+
+                    Class<?> c = cl.loadClass(className);
+
+                    Runnable r = (Runnable) c.getDeclaredConstructor().newInstance();
+                    r.run();
+
+                    final ExecutionDataStore executionData = new ExecutionDataStore();
+                    final SessionInfoStore sessionInfos = new SessionInfoStore();
+                    data.collect(executionData, sessionInfos, false);
+                    runtime.shutdown();
+
+                    final CoverageBuilder coverageBuilder = new CoverageBuilder();
+                    final Analyzer analyzer = new Analyzer(executionData, coverageBuilder);
+                    InputStream original = c.getResourceAsStream(name);
+                    analyzer.analyzeClass(original, className);
+                    original.close();
                 }
             }
         }
+    }
+
+    public void gC() {
+        System.out.println(getClass());
     }
 
 }
