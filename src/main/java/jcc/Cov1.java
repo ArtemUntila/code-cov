8d33f878e66bdc472ce946e861041cad78474a08
Artyom-IWT <untila_artem@mail.ru>
2021-04-10 22:34:33 +0300

    ......

--- src/main/java/jcc/Cov1.java
+++ src/main/java/jcc/Cov1.java
@@ -11,18 +11,37 @@ import org.jacoco.core.runtime.LoggerRuntime;
 import org.jacoco.core.runtime.RuntimeData;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.Enumeration;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 public class Cov1 {
 
-    public static void main(String[] args) throws Exception {
+    public static class MemoryClassLoader extends ClassLoader {
+
+        private final Map<String, byte[]> definitions = new HashMap<>();
+
+        public void addDefinition(final String name, final byte[] bytes) {
+            definitions.put(name, bytes);
+        }
+
+        @Override
+        public Class<?> loadClass(final String name)
+                throws ClassNotFoundException {
+            final byte[] bytes = definitions.get(name);
+            if (bytes != null) {
+                return defineClass(name, bytes, 0, bytes.length);
+            }
+            return super.loadClass(name);
+        }
+
+    }
+
+    public void execute(String[] args) throws Exception {
         if (args == null || args.length < 1)
             throw new IllegalArgumentException("Incorrect data entered");
 
@@ -38,27 +57,49 @@ public class Cov1 {
 
         String[] scanned = scanner.getIncludedFiles();
 
-        URL[] urls = new URL[scanned.length];
-        List<JarFile> jars = new LinkedList<>();
+        JarFile[] jars = new JarFile[scanned.length];
         String projectPath = project.getAbsolutePath();
-
+        StringBuilder classPath = new StringBuilder(System.getProperty("java.class.path"));
+        System.out.println(System.getProperty("java.class.path"));
         for (int i = 0; i < scanned.length; i++) {
-            String p = projectPath + "\\" + scanned[i];
+            String p = projectPath + "\\" + scanned[i]; // полный путь к jar-файлу
             System.out.println(p);
-            urls[i] = new URL("file:" + p);
-            jars.add(new JarFile(p));
+            jars[i] = new JarFile(p);
+            classPath.append(';').append(p);
         }
+        System.setProperty("java.class.path", classPath.toString());
+        //System.out.println(System.getProperty("java.class.path"));
+        //MemoryClassLoader mCL = new MemoryClassLoader();
+        //mCL.addDefinition(".tests.TestAdder", null);
+        //mCL.addDefinition(".classes.Adder", null);
+        //Class<?> c = mCL.loadClass(".tests.TestAdder");
+        //Runnable r = (Runnable) c.getDeclaredConstructor().newInstance();
+        //r.run();
 
-        URLClassLoader cl = new URLClassLoader(urls);
+        final IRuntime runtime = new LoggerRuntime();
 
-        for (JarFile jar : jars) {
+        Instrumenter instr = new Instrumenter(runtime);
+        InputStream original = this.getClass().getClassLoader().getResourceAsStream("/classes/Adder.class"); // getClass() isn't static
+        if (original == null) {
+            System.err.println("OF COURSE");
+            //final byte[] instrumented = instr.instrument(original, "classes.Adder");
+        } else {
+            System.out.println("NO WAY");
+            original.close();
+        }
+        System.out.println(System.class.getClassLoader().getResource("."));
+
+        /*ClassLoader cl = ClassLoader.getSystemClassLoader();
+        cl.loadClass("Adder");*/
+        /*for (JarFile jar : jars) {
             Enumeration<JarEntry> e = jar.entries();
-            while (e.hasMoreElements()) {
+            while (e.hasMoreElements()) {Ф
                 JarEntry je = e.nextElement();
-                String name = je.getName();
+                //String name = je.getName();
+                System.out.println(je);*/
+                /*System.out.println(name + " - " + getCanonicalName(name));
                 if (name.endsWith(".class") && name.contains("tests/")) {
                     System.out.println(name); //"/tests/TestAdder.class"
-                    //"tests.TestAdder"
                     String className = name.substring(0, je.getName().length() - 6).replace('/', '.');
                     System.out.println(className);
                     /*Arrays.stream(c.getMethods()).forEach(System.out::println);
@@ -72,18 +113,18 @@ public class Cov1 {
                     //System.out.println("\nRunning test...\n");
 
 
-                    final IRuntime runtime = new LoggerRuntime();
+                    /*final IRuntime runtime = new LoggerRuntime();
 
-                    //Instrumenter instr = new Instrumenter(runtime);
-                    //InputStream original = new Cov1().getClass().getResourceAsStream(name);
-                    //final byte[] instrumented = instr.instrument(original, className);
-                    //original.close();
+                    Instrumenter instr = new Instrumenter(runtime);
+                    InputStream original = System.class.getResourceAsStream(className + ".class"); // getClass() isn't static
+                    final byte[] instrumented = instr.instrument(original, className);
+                    original.close();
 
                     final RuntimeData data = new RuntimeData();
                     runtime.startup(data);
 
                     Class<?> c = cl.loadClass(className);
-
+                    System.out.println(ClassLoader.getSystemClassLoader());
                     Runnable r = (Runnable) c.getDeclaredConstructor().newInstance();
                     r.run();
 
@@ -94,16 +135,15 @@ public class Cov1 {
 
                     final CoverageBuilder coverageBuilder = new CoverageBuilder();
                     final Analyzer analyzer = new Analyzer(executionData, coverageBuilder);
-                    InputStream original = c.getResourceAsStream(name);
+                    original = c.getResourceAsStream(name);
                     analyzer.analyzeClass(original, className);
-                    original.close();
-                }
-            }
-        }
+                    original.close();*/
+                //}
+            //}
+        //}
     }
 
-    public void gC() {
-        System.out.println(getClass());
+    public static void main(String[] args) throws Exception{
+        new Cov1().execute(args);
     }
-
 }
